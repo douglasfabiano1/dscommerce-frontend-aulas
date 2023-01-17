0cfd46cc8656cf50db24af088138eabcbfdb438a
Douglas Fabiano <douglasfabiano23@gmail.com>
2023-01-17 13:53:10 -0300

    Project clean

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render( // function render do the renderization and produce the result on the screen.
   <React.StrictMode>
     <App />
   </React.StrictMode>,
