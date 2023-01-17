50da4eced9feb4d5ced654057dfa391a2cfbdd30
Douglas Fabiano <douglasfabiano23@gmail.com>
2023-01-17 17:44:16 -0300

    React first render test

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render( // function render do the renderization and produce the result on the screen.
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+// this is the entrypoint of the react function.
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  // function render do the renderization and produce the result on the screen.
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
